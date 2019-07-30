18f9d61b5b6e63ca9a673c971e02fd2331b967a5
André Vieira <Andr Vieira@DESKTOP-S2BVFB7>
2019-07-30 12:36:17 +0000

    Test Suite Directory Andre

---
+++ Test Suites/Directory/EmployeeList.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>EmployeeList</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e8ab0b9d-e8c6-44c0-bb1d-498c82abd1d3</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Directory/MyProfile.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MyProfile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e837fda6-b079-4e26-aa78-2d022ef61679</testSuiteGuid>
+</TestSuiteEntity>
