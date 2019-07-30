490fdc5e3fe416e4c297d5422ab0d6c0b10b319f
Laura Fidalgo <Laura Fidalgo@DESKTOP-H1U2MR0.lan>
2019-07-30 12:05:47 +0100

    Allocation tests

---
+++ Scripts/Allocation/Allocation/Script1564484519678.groovy
@@ -0,0 +1,44 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://internal.itup.pt/Menu_HRPortal/')
+
+WebUI.setText(findTestObject('Page_Login/input_Remember login_WebPatterns_wt11blockwtUsernamewtUserNameInput'), 'laura.fidalgo.dev')
+
+WebUI.setEncryptedText(findTestObject('Page_Login/input_Remember login_WebPatterns_wt11blockwtPasswordwtPasswordInput'), 
+    'Y1Nh4jfnDFCge6QYsO1dtg==')
+
+WebUI.click(findTestObject('Page_Login/input_Remember login_WebPatterns_wt11blockwtActionwtLoginButton'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_HomePage/div_AllocationManage'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/ul_Employee_select2-choices'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/input_label_ITUpTheme_wt148blockwtMainContentwt142'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/ui-autocomplete-item_201801-NOE-TRN'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/a_Ana Reis_select2-search-choice-close'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/input_label_ITUpTheme_wt148blockwtMainContentwt142'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/input_Current Week_ITUpTheme_wt148blockwtMainContentwt8'))
+
+WebUI.click(findTestObject('Object Repository/Repository_Allocation/Page_AllocationCalendar/input_label_ITUpTheme_wt148blockwtMainContentwt50'))
+
+WebUI.closeBrowser()
+
