c571824b3901eec3e345d8db18be5282ef493d30
André Vieira <Andr Vieira@DESKTOP-S2BVFB7>
2019-07-30 12:44:52 +0000

    Directory Andre

--- Test Suites/Directory/MyProfile.ts
+++ Test Suites/Directory/MyProfile.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>e837fda6-b079-4e26-aa78-2d022ef61679</testSuiteGuid>
+   <testCaseLink>
+      <guid>2942934b-132a-4b3a-81dd-4568ae1fd2ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Directory/MyProfile/Test MyProfile</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cc750fda-b370-4d24-b6a5-ac49871af46f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Directory/MyProfile/Test MyProfile_Edit</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
