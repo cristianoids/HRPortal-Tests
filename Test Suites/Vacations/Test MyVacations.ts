b2a105ca45529cfa179f8e44729d03359e37a894
André Vieira <Andr Vieira@DESKTOP-S2BVFB7>
2019-07-30 09:50:16 +0000

    Vacations SuiteUpdate

---
+++ Test Suites/Vacations/Test MyVacations.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test MyVacations</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>949b5331-c5cb-4e80-9985-794f18e4621f</testSuiteGuid>
+</TestSuiteEntity>
