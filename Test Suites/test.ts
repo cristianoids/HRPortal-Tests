d2ab893fc5a4d99feb89f2c4547a9380e5e29e46
André Vieira <Andr Vieira@DESKTOP-S2BVFB7>
2019-08-01 14:14:39 +0000

    test

---
+++ Test Suites/test.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5a87aa79-2bd8-4e48-96b7-067697fb7c56</testSuiteGuid>
+</TestSuiteEntity>
