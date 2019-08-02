551d751b3abc86b6a9c6d88a275b1ba9c984c037
André Vieira <Andr Vieira@DESKTOP-S2BVFB7>
2019-08-02 09:53:21 +0000

    test andre laura

---
+++ Object Repository/delete2/Page_MyTimeSheet/span_Approve Time Sheets_fa fa-fw fa-check-circle.rs
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Approve Time Sheets_fa fa-fw fa-check-circle</name>
+   <tag></tag>
+   <elementGuidId>51a00219-7465-4772-a5f8-7f578339fd35</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='ITUpTheme_wt17_block_wtMenu_Menu_HRPortal_wt58_block_RichWidgets_wtApproveTimeSheet_block_wtMenuItem_wt180']/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fa fa-fw fa-check-circle</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ITUpTheme_wt17_block_wtMenu_Menu_HRPortal_wt58_block_RichWidgets_wtApproveTimeSheet_block_wtMenuItem_wt180&quot;)/span[@class=&quot;fa fa-fw fa-check-circle&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='ITUpTheme_wt17_block_wtMenu_Menu_HRPortal_wt58_block_RichWidgets_wtApproveTimeSheet_block_wtMenuItem_wt180']/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Approve Time Sheets'])[1]/span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='My Time Sheet'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Management'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='RowData'])[1]/preceding::span[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/a/span</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/Test/Script1564739467153.groovy
@@ -0,0 +1,54 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://internal.itup.pt/Menu_HRPortal/NoPermission.aspx')
+
+WebUI.setText(findTestObject('Object Repository/delete2/Page_Login/input_Remember login_WebPatterns_wt11blockwtUsernamewtUserNameInput'), 
+    'andre.vieira.dev')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/delete2/Page_Login/input_Remember login_WebPatterns_wt11blockwtPasswordwtPasswordInput'), 
+    'aeHFOx8jV/A=')
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_Login/input_Remember login_WebPatterns_wt11blockwtActionwtLoginButton'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/delete2/Page_Login/input_Remember login_WebPatterns_wt11blockwtPasswordwtPasswordInput'), 
+    'Y1Nh4jfnDFCge6QYsO1dtg==')
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_Login/input_Remember login_WebPatterns_wt11blockwtActionwtLoginButton'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_HomePage/span_Time Sheets'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_MyTimeSheet/span_Approve Time Sheets_fa fa-fw fa-check-circle'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_ListOfEmployeesToApproveTS/span_Management_fa fa-fw fa-gears'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_ListOfEmployeesToApproveTS/a_RowData'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_RowData/span_Management_fa fa-fw fa-gears'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_RowData/span_Management_fa fa-fw fa-gears'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_RowData/a_Report'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_Report/span_Management_fa fa-fw fa-gears'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_Report/span_Management_fa fa-fw fa-gears'))
+
+WebUI.click(findTestObject('Object Repository/delete2/Page_Report/a_Timesheet Approved'))
+
+WebUI.closeBrowser()
+
